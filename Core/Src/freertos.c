c019fca14bdd1996c632b4e0acadc89d4be69e55
Hotakus <553070703@qq.com>
2021-10-06 03:24:54 +0800

    upload file

---
+++ Core/Src/freertos.c
@@ -0,0 +1,221 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2021 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under Ultimate Liberty license
+  * SLA0044, the "License"; You may not use this file except in compliance with
+  * the License. You may obtain a copy of the License at:
+  *                             www.st.com/SLA0044
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "lv_port_disp_template.h"
+#include <st7735.h>
+#include <lvgl.h>
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+
+/* USER CODE END Variables */
+/* Definitions for defaultTask */
+osThreadId_t defaultTaskHandle;
+const osThreadAttr_t defaultTask_attributes = {
+  .name = "defaultTask",
+  .stack_size = 128 * 4,
+  .priority = (osPriority_t) osPriorityNormal,
+};
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void *argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* Hook prototypes */
+void configureTimerForRunTimeStats(void);
+unsigned long getRunTimeCounterValue(void);
+
+/* USER CODE BEGIN 1 */
+/* Functions needed when configGENERATE_RUN_TIME_STATS is on */
+__weak void configureTimerForRunTimeStats(void)
+{
+
+}
+
+__weak unsigned long getRunTimeCounterValue(void)
+{
+return 0;
+}
+
+
+#define LVGL_TICK 5	// ms
+
+static osThreadId_t lvgl_tick_task_handle;
+const osThreadAttr_t lvgl_tick_task_attrs  = {
+  .name = "lvgl_tick",
+  .stack_size = 1024,
+  .priority = (osPriority_t) osPriorityLow1,
+};
+static void lvgl_tick_task()
+{
+	while(1) {
+		lv_tick_inc(LVGL_TICK);
+		osDelay(LVGL_TICK);
+	}
+}
+
+static osThreadId_t lvgl_task_handle;
+const osThreadAttr_t lvgl_task_attrs  = {
+  .name = "lvgl",
+  .stack_size = 4096,
+  .priority = (osPriority_t) osPriorityLow,
+};
+static void lvgl_task()
+{
+	while(1) {
+		lv_timer_handler();
+		osDelay(LVGL_TICK);
+	}
+}
+
+static void lvgl_task_init()
+{
+	lvgl_tick_task_handle = osThreadNew(lvgl_tick_task, NULL, &lvgl_tick_task_attrs);
+	lvgl_task_handle = osThreadNew(lvgl_task, NULL, &lvgl_task_attrs);
+}
+
+static void lv_ex_label(void)
+{
+	static char* github_addr = "";
+	lv_obj_t * label = lv_label_create(lv_scr_act());
+    lv_label_set_recolor(label, true);
+    lv_label_set_long_mode(label, LV_LABEL_LONG_SCROLL_CIRCULAR); /*Circular scroll*/
+    lv_obj_set_width(label, 120);
+    lv_label_set_text_fmt(label, "#ff0000 Github: %s#", github_addr);
+    lv_obj_align(label, LV_ALIGN_CENTER, 0, 10);
+	
+    lv_obj_t * label2 = lv_label_create(lv_scr_act());
+    lv_label_set_recolor(label2, true);
+    lv_label_set_long_mode(label2, LV_LABEL_LONG_SCROLL_CIRCULAR); /*Circular scroll*/
+    lv_obj_set_width(label2, 120);
+    lv_label_set_text_fmt(label2, "#ff0000 Hello# #00ff00 world ! Trisuborn.#");
+    lv_obj_align(label2, LV_ALIGN_CENTER, 0, -10);
+}
+
+static void lvgl_init()
+{
+	lv_init();
+	lv_port_disp_init();
+	lv_ex_label();
+	
+	lvgl_task_init();
+}
+
+/* USER CODE END 1 */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of defaultTask */
+  defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void *argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+	
+	
+	lvgl_init();
+	
+	
+  /* Infinite loop */
+  for(;;)
+  {
+	  HAL_GPIO_TogglePin(LED_GPIO_Port, LED_Pin);
+    osDelay(200);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
